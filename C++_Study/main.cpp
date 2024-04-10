9b5f7cafb2cec7f4a8cdaa2b5136e5896543836b
toki1563 <sanda1563@icloud.com>
2024-04-10 18:59:02 +0900

    4/10 P80
    構造体・共用体・列挙体まで

--- C++_Study/main.cpp
+++ C++_Study/main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 
+#if false
+
+// 定数と参考条件演算子
 int main()
 {
 	// 定数
 	// 初めに設定した数字のみ使用可能
 	// 後に代入は不可
-	//const int a = 5;
-
+	const int a = 5;
 
 	// 参考条件演算子
-	//bool isChack = false;
-	//int a = 0;
-	//isChack ? a = 1 : a = 5;
-	//std::cout << a << std::endl;
-
-
-
-
-
+	bool isChack = false;
+	int a = 0;
+	isChack ? a = 1 : a = 5;
+	std::cout << a << std::endl;
 
 	return 0;
 }
 
+
+
+#elif true
+
+// 列挙型
+enum class Categry
+{
+	Value1,
+	Value2,
+	Value3 = 100,
+	Value4,
+};
+
+int main()
+{
+	Categry cat = Categry::Value3;
+
+	std::cout << static_cast<int>(cat) << std::endl;
+}
+
+#endif
